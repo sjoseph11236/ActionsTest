058e6c62be3c9f62490e02cd56dff08a8e871af9
SayeedJ <sayeedjoseph@gmail.com>
2020-11-09 10:46:14 -0500

    add another test

---
+++ src/test/java/OtherTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class OtherTest {
+
+    @Test
+    public void test() {
+        fail("not yet implemented");
+    }
+
+}
