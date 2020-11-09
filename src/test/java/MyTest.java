5c8087779853d2624d1458492b2bf03cbbabdffb
SayeedJ <sayeedjoseph@gmail.com>
2020-11-09 10:43:09 -0500

    passing test

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static com.sun.tools.internal.ws.wsdl.parser.Util.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyTest {
+
+    @Test
+    public void test() {
+        assertEquals(5, 5);
+    }
+
+}
